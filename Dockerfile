448ec57c8fe77ae5c536ad60bb0ffe380d098a0d
cb-shivam <98250333+cb-shivam@users.noreply.github.com>
2022-03-29 13:38:27 +0530

    Initial Commit

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM --platform=linux/amd64 openjdk:8-jdk-alpine
+ADD target/HelloWorld-0.0.1-SNAPSHOT.jar ap.jar
+EXPOSE 80
+ENTRYPOINT ["java", "-jar","/ap.jar"]
---
+++ src/main/java/com/example/helloworld/controllers/HelloWorldController.java
@@ -0,0 +1,12 @@
+package com.example.helloworld.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @GetMapping("/")
+    public String viewAllTransactions() {
+        return "Hey There";
+    }
+}
