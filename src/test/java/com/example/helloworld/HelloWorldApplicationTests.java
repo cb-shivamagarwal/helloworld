6ab9b048e172e9bf2ed9dfd3a5c8d4bba97763b3
cb-shivam <98250333+cb-shivam@users.noreply.github.com>
2022-03-30 15:36:43 +0530

    Added database

--- src/test/java/com/example/helloworld/HelloWorldApplicationTests.java
+++ src/test/java/com/example/helloworld/HelloWorldApplicationTests.java
@@ -1,13 +1,22 @@
 package com.example.helloworld;
 
+import com.example.helloworld.controllers.HelloWorldController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class HelloWorldApplicationTests {
 
+	@Autowired
+	private HelloWorldController controller;
+
 	@Test
-	void contextLoads() {
+	void contextLoads() throws Exception {
+		assertThat(controller).isNotNull();
 	}
 
 }
